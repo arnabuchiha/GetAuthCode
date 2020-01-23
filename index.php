63c1044ad503044bfd5a71b36a03d6d253747ff9
arnabuchiha <arnabkeya04@gmail.com>
2020-01-23 14:02:32 +0530

    Done changes

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
  if(isset($_GET['code'])){
     setcookie("auth_code",substr($_GET['code'],0,-2),time() + (86400 * 30),"/");
-    header("Location:http://localhost:3000/"); 
+    header("Location:http://localhost:3000/");
  }
  else{
     setcookie("auth_code","error",time() + (86400 * 30),"/");
+    header("Location:http://localhost:3000/");
  }
  echo $_COOKIE["auth_code"];
 ?>
