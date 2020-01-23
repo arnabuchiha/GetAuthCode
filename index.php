a1487725bf005d015911c32a02118e9d9c75eb59
arnabuchiha <arnabkeya04@gmail.com>
2020-01-23 14:44:44 +0530

    Changes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     header("Location:http://localhost:3000/"."code/".substr($_GET['code'],0,-2));
  }
  else{
-    header("Location:http://localhost:3000/"."code/error");
+    header("Location:http://localhost:3000/"."code/error".substr($_GET['code'],0,-2));
  }
 ?>
 
