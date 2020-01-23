d0f7903449ca962d145a33dc2887c4ea08900a26
arnabuchiha <arnabkeya04@gmail.com>
2020-01-23 13:45:28 +0530

    Done

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+ if(isset($_GET['code'])){
+    setcookie("auth_code",substr($_GET['code'],0,-2),time() + (86400 * 30),"/");
+    header("Location:http://localhost:3000/"); 
+ }
+ else{
+    setcookie("auth_code","error",time() + (86400 * 30),"/");
+ }
+ echo $_COOKIE["auth_code"];
+?>
